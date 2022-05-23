a0127332dcd25accb35f47037254843791f6f448
berke.ayan <82400213+SeeMeInCrown@users.noreply.github.com>
2022-05-23 23:19:43 +0300

    paper service working

--- src/main/java/net/conferencescheduling/spring/controller/ConstraintController.java
+++ src/main/java/net/conferencescheduling/spring/controller/ConstraintController.java
@@ -1,5 +1,7 @@
 package net.conferencescheduling.spring.controller;
 
+import net.conferencescheduling.spring.model.dto.ConstraintDto;
+import net.conferencescheduling.spring.model.dto.PaperDto;
 import net.conferencescheduling.spring.model.entity.Constraint;
 import net.conferencescheduling.spring.service.ConstraintService;
 import org.modelmapper.ModelMapper;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/constraint")
@@ -30,7 +34,19 @@ public class ConstraintController {
 
         return new ResponseEntity<>(constraintResponse, HttpStatus.CREATED);
     }
+    @GetMapping("/getAll")
+    public ResponseEntity<List<ConstraintDto>> getAllConstraints() {
+        try {
+            List<ConstraintDto> papers = constraintService.getAllConstraints().stream().map(constraint -> modelMapper.map(constraint, ConstraintDto.class)).toList();
+            if (papers.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return ResponseEntity.ok(papers);
 
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     @DeleteMapping("/deleteAll")
     public ResponseEntity<HttpStatus> deleteAllPapers() {
         try {
